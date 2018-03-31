7a98b00b946457e0dd3eeae05432a97672c4825a
Israel de S. Carlos <rael.comp@gmail.com>
2018-03-31 19:18:32 -0300

    Versao inical do projeto

---
+++ src/main/java/com/israel/api/MeuPrimeiroProjetoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.israel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+	}
+}
