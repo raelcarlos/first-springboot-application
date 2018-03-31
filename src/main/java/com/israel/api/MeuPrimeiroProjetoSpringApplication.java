84cc77a1e3b125b08aa572515dc8193fb83345df
Israel de S. Carlos <rael.comp@gmail.com>
2018-03-31 19:25:08 -0300

    Alteracao no arquivo principal da aplicacao

--- src/main/java/com/israel/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/com/israel/api/MeuPrimeiroProjetoSpringApplication.java
@@ -8,5 +8,6 @@ public class MeuPrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+		System.out.println("Minha primeira aplicacao.");
 	}
 }
